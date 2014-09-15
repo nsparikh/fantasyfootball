1bded759c04bf060c16324205c8b119695c8d946
Neena Parikh <nparikh92@gmail.com>
2014-09-15 16:22:56 -0400

    Minor fixes

--- game/static/game/players.js
+++ game/static/game/players.js
@@ -1,34 +1,34 @@
 var columns = [
-	"name", "team", "position", 
-	"passC/passA", "passYds", "passTDs", "passInt",
-	"rush", "rushYds", "rushTDs",
-	"rec", "recYds", "recTDs", "recTar",
-	"misc2pc", "miscFuml", "miscTDs", "points"
+	'name', 'team', 'position', 
+	'passC/passA', 'passYds', 'passTDs', 'passInt',
+	'rush', 'rushYds', 'rushTDs',
+	'rec', 'recYds', 'recTDs', 'recTar',
+	'misc2pc', 'miscFuml', 'miscTDs', 'points'
 ];
 
 // Colors
-var blue = "#21BDCD";
-var white = "#D4D4D4"
-var darkGray = "#232323";
+var blue = '#21BDCD';
+var white = '#D4D4D4'
+var darkGray = '#232323';
 
-var table = d3.select("#scoring-leaders-table");
+var table = d3.select('#scoring-leaders-table');
 
 // Add rows to table
-var tbody = table.append("tbody");
-var rows = tbody.selectAll("tr")
+var tbody = table.append('tbody');
+var rows = tbody.selectAll('tr')
 	.data(dataset)
 	.enter()
-	.append("tr")
-	.attr("class", "table-small visible");
-var rowData = rows.selectAll("td")
+	.append('tr')
+	.attr('class', 'table-small visible');
+var rowData = rows.selectAll('td')
 	.data(function(row) {
 		return columns.map(function(column) {
 			var value;
-			if (column == "name") {
+			if (column == 'name') {
 				value = row['player']['name']; 
-			} else if (column == "team" || column == "position") {
-				value = row['player'][column]['abbr'];
-			} else if (column == "passC/passA") {
+			} else if (column == 'team' || column == 'position') {
+				value = row['player'][column]['abbr'].toUpperCase();
+			} else if (column == 'passC/passA') {
 				if (row['data']['passC'] == null) value = '-- / --';
 				else value = row['data']['passC']+' / '+row['data']['passA'];
 			} else value = row['data'][column]
@@ -37,26 +37,24 @@ var rowData = rows.selectAll("td")
 	}).enter();
 
 // Filter data to add links to the player names / teams / positions
-rowData.append("td")
+rowData.append('td')
 	.text(function(d) {
-		if (d.column != "name" && d.column != "team" && d.column != "position") {
-			if (d.value == null) return "--";
+		if (d.column != 'name' && d.column != 'team' && d.column != 'position') {
+			if (d.value == null) return '--';
 			else return d.value;
 		}
 		return null;
 	})
 	.attr('class', 'td-wide')
 	.filter(function(d) {
-		return d.column == "name" || d.column == "team" || d.column == "position";
+		return d.column == 'name' || d.column == 'team' || d.column == 'position';
 	})
-	.append("a")
-	.attr("href", function(d) {
-		if (d.column == "name") return ""+d.player.id;
-		else if (d.column == "team") return team_url;
-		else if (d.column == "position") return position_url+d.player.position.id;
+	.append('a')
+	.attr('href', function(d) {
+		if (d.column == 'name') return ''+d['player']['id'];
+		else if (d.column == 'team') return team_url+d['player']['team']['id'];
+		else if (d.column == 'position') return position_url+d['player']['position']['id'];
 		return null;
 	})
-	.attr("class", "white-link")
+	.attr('class', 'white-link')
 	.text(function(d) { return d.value; });
-
-// TODO: underline selected position name, selected column name
