64e9eb663a786c71e71b5317b6785f3690a8ee1b
Neena Parikh <nparikh92@gmail.com>
2014-10-22 16:58:43 -0400

    Remove Yahoo API verification file

--- football/urls.py
+++ football/urls.py
@@ -9,7 +9,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'fantasyfootball.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^YuX85.hPUXs22k6fEvlyMflBMJlMOnTVYWHSJN1N8g--.html', game.views.yahoo_api_verify),
     url(r'^$', lambda r : HttpResponseRedirect('players/')),
     url(r'', include('game.urls', namespace='game')),
     url(r'^admin/', include(admin.site.urls)),
