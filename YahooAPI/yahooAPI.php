d566974eb18eddbeb3b2729bc68ee54b93dae4c9
Neena Parikh <nparikh92@gmail.com>
2014-11-04 13:23:55 -0500

    Get more 2012 data

--- YahooAPI/yahooAPI.php
+++ YahooAPI/yahooAPI.php
@@ -98,12 +98,10 @@ function writeGameData($year) {
 
     // Go through each player
     foreach ($GLOBALS['players_json'] as $player_index=>$player) {
-        //if ($player_index < 1409) continue;
+        if ($player_index < 1000) continue;
 
         // Go through each week
         foreach (range(1, 17) as $week_num) {
-            if ($player_index < 703) continue;
-
             // Get the data for the given week
             print $player_index.'/1485 '.$player['pk'].' '.$player['fields']['name'] . ' W' . $week_num . ' ';
 
